f3a0d8dfd74c182a36aaa4642457f40e5ee127a5
Steffen Winkler <nospam{cpanMINUS94k2fp2eATsteffenMINUSwinklerDOTde}@example.com>
2009-06-02 20:16:06 +0000

    Import of STEFFENW/Object-Lazy-0.06 from CPAN.
    gitpan-cpan-distribution: Object-Lazy
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         STEFFENW/Object-Lazy-0.06.tar.gz
    gitpan-cpan-author:       STEFFENW
    gitpan-cpan-maturity:     released

--- lib/Object/Lazy.pm
+++ lib/Object/Lazy.pm
@@ -3,7 +3,7 @@ package Object::Lazy;
 use strict;
 use warnings;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use Carp qw(confess);
 use English qw(-no_match_vars $EVAL_ERROR);
@@ -90,7 +90,7 @@ Object::Lazy - create objects late from non-owned classes
 
 =head1 VERSION
 
-0.05
+0.06
 
 =head1 SYNOPSIS
 
